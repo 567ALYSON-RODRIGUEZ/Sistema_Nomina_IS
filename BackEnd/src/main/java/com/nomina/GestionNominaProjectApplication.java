cb9bda5b25182426d0f6ac2bb325ec6389599881
vanne <arodriguezq3@miumg.edu.gt>
2025-04-18 12:47:42 -0600

    Base del proyecto con frontend y backend

---
+++ BackEnd/src/main/java/com/nomina/GestionNominaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.nomina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionNominaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionNominaProjectApplication.class, args);
+	}
+
+}
