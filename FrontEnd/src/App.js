6b8e2937eb47ed128ae6f11b08976f80fb844cb1
vboxuser <maria>
2025-05-24 17:04:14 -0600

    correcciones de app,js y sidebar

---
+++ FrontEnd/src/App.js
@@ -0,0 +1,251 @@
+import React, { useState } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+import EmpleadoCrud from './components/EmpleadoCrud';
+import PeriodoNomina from './components/PeriodoNomina';
+import DetalleConceptos from './components/DetalleConceptos';
+import Departamentos from './components/Departamentos';
+import Roles from './components/Roles';
+import Puestos from './components/Puestos';
+import Empresa from './components/Empresa';
+import Usuarios from './components/Usuarios';
+import Sidebar from './components/Sidebar';
+import Anticipos from './components/Anticipos';
+import Asistencias from './components/Asistencias';
+import NominasE from './components/NominasE';
+import Bonificacion from './components/Bonificacion'
+import ParametroIgss from './components/ParametroIgss';
+import Mensual from './components/Mensual'
+import Quincenal from './components/Quincenal';
+import Semanal from './components/Semanal';
+import Login from './components/Login';
+import Permisos from './components/Permisos';
+import Isr from './components/Isr';
+import PlanillaAguinaldo from './components/PlanillaAguinaldo';
+import PlanillaBono14 from './components/PlanillaBono14';
+import Finiquito from './components/Finiquito';
+
+import { useNavigate } from 'react-router-dom';
+
+import './App.css';
+
+function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [role, setRole] = useState('');
+  const navigate = useNavigate();
+
+  const ProtectedLayout = ({ children }) => {
+    if (!isAuthenticated) return <Navigate to="/login" replace />;
+    return (
+      <div className="app-container">
+        <Sidebar role={role} onSelect={(ruta) => navigate(`/${ruta}`)} />
+        <div className="main-content">{children}</div>
+      </div>
+    );
+  };
+
+  return (
+    <Routes>
+      <Route
+        path="/login"
+        element={
+          isAuthenticated ? (
+            <Navigate to="/" replace />
+          ) : (
+            <Login setIsAuthenticated={setIsAuthenticated} setRole={setRole} />
+          )
+        }
+      />
+      <Route
+        path="/"
+        element={
+          <ProtectedLayout>
+            <h2>Bienvenida</h2>
+          </ProtectedLayout>
+        }
+      />
+      <Route
+        path="/empleado"
+        element={
+          <ProtectedLayout>
+            <EmpleadoCrud />
+          </ProtectedLayout>
+        }
+      />
+
+      <Route
+        path="/periodo"
+        element={
+          <ProtectedLayout>
+            <PeriodoNomina />
+          </ProtectedLayout>
+        }
+      />
+      <Route
+        path="/detalle"
+        element={
+          <ProtectedLayout>
+            <DetalleConceptos />
+          </ProtectedLayout>
+        }
+      />
+      <Route
+        path="/rol"
+        element={
+          <ProtectedLayout>
+            <Roles />
+          </ProtectedLayout>
+        }
+      />
+      <Route
+        path="/puesto"
+        element={
+          <ProtectedLayout>
+            <Puestos />
+          </ProtectedLayout>
+        }
+      />
+      <Route
+        path="/departamento"
+        element={
+          <ProtectedLayout>
+            <Departamentos />
+          </ProtectedLayout>
+        }
+      />
+      <Route
+        path="/empresa"
+        element={
+          <ProtectedLayout>
+            <Empresa />
+          </ProtectedLayout>
+        }
+      />
+      <Route
+        path="/usuario"
+        element={
+          <ProtectedLayout>
+            <Usuarios />
+          </ProtectedLayout>
+        }
+      />
+
+      
+      <Route
+        path="/permisos"
+        element={
+          <ProtectedLayout>
+            <Permisos />
+          </ProtectedLayout>
+        }
+      />
+      <Route
+        path="/isr"
+        element={
+          <ProtectedLayout>
+            <Isr />
+          </ProtectedLayout>
+        }
+      />
+      <Route
+        path="/planilla"
+        element={
+          <ProtectedLayout>
+            <PlanillaAguinaldo />
+          </ProtectedLayout>
+        }
+      />
+      <Route
+        path="/planilla14"
+        element={
+          <ProtectedLayout>
+            <PlanillaBono14 />
+          </ProtectedLayout>
+        }
+      />
+      <Route
+        path="/finiquito"
+        element={
+          <ProtectedLayout>
+            <Finiquito />
+          </ProtectedLayout>
+        }
+      />
+
+
+    <Route
+        path="/anticipo"
+        element={
+          <ProtectedLayout>
+            <Anticipos />
+          </ProtectedLayout>
+        }
+      />
+
+          <Route
+        path="/bonificacion"
+        element={
+          <ProtectedLayout>
+            <Bonificacion />
+          </ProtectedLayout>
+        }
+      />
+
+                <Route
+        path="/asistencia"
+        element={
+          <ProtectedLayout>
+            <Asistencias />
+          </ProtectedLayout>
+        }
+      />
+
+                <Route
+        path="/nominas"
+        element={
+          <ProtectedLayout>
+            <NominasE />
+          </ProtectedLayout>
+        }
+      />
+
+                <Route
+        path="/igss"
+        element={
+          <ProtectedLayout>
+            <ParametroIgss />
+          </ProtectedLayout>
+        }
+      />
+
+      <Route
+        path="/detallenominaM"
+        element={
+          <ProtectedLayout>
+            <Mensual />
+          </ProtectedLayout>
+        }
+      />
+
+      <Route
+        path="/detallenominaQ"
+        element={
+          <ProtectedLayout>
+            <Quincenal />
+          </ProtectedLayout>
+        }
+      />
+
+      <Route
+        path="/detallenominaS"
+        element={
+          <ProtectedLayout>
+            <Semanal />
+          </ProtectedLayout>
+        }
+      />
+    </Routes>
+  );
+}
+
+export default App;
---
+++ FrontEnd/src/components/Sidebar.jsx
@@ -0,0 +1,104 @@
+import React, { useState, useEffect } from 'react';
+import './Sidebar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faUsers, faBriefcase, faBuilding, faUserShield, faMoneyCheckAlt,
+  faCalendarAlt, faBars, faChevronLeft, faChevronDown, faChevronUp,
+  faLandmark, faClock, faUser, faUniversity, faGift, faFileInvoiceDollar,
+  faLayerGroup, faSignOutAlt
+} from '@fortawesome/free-solid-svg-icons';
+
+function Sidebar({ onSelect }) {
+  const [open, setOpen] = useState(true);
+  const [mantenimientosOpen, setMantenimientosOpen] = useState(false);
+  const [nominaOpen, setNominaOpen] = useState(false);
+  const [role, setRole] = useState('');
+
+  useEffect(() => {
+    const storedRole = localStorage.getItem('role');
+    console.log('Rol desde localStorage:', storedRole);
+    setRole(storedRole);
+  }, []);
+
+  const handleClick = (option) => {
+    onSelect(option);
+    setOpen(false);
+  };
+
+  const handleLogout = () => {
+    localStorage.clear();
+    window.location.reload();
+  };
+
+  return (
+    <div className={`sidebar ${open ? 'open' : 'closed'}`}>
+      <div className="toggle-button" onClick={() => setOpen(!open)}>
+        {open ? <FontAwesomeIcon icon={faChevronLeft} /> : <FontAwesomeIcon icon={faBars} />}
+      </div>
+
+      {open && (
+        <ul className="menu">
+          
+          <li className="info-message">
+          Has iniciado sesión como: <strong>{role}</strong>
+          </li>
+
+          {(role === 'Admin' || role === 'RRHH') && (
+            <>
+              <li onClick={() => setMantenimientosOpen(!mantenimientosOpen)}>
+                <FontAwesomeIcon icon={faLayerGroup} /> Mantenimientos {mantenimientosOpen ? <FontAwesomeIcon icon={faChevronUp} /> : <FontAwesomeIcon icon={faChevronDown} />}
+              </li>
+              {mantenimientosOpen && (
+                <ul className="submenu">
+                  <li onClick={() => handleClick('empleado')}><FontAwesomeIcon icon={faUsers} /> Empleados</li>
+                  {role === 'Admin' && (
+                    <>
+                      <li onClick={() => handleClick('usuario')}><FontAwesomeIcon icon={faUser} /> Usuarios</li>
+                      <li onClick={() => handleClick('empresa')}><FontAwesomeIcon icon={faUniversity}/> Empresa</li>
+                      <li onClick={() => handleClick('puesto')}><FontAwesomeIcon icon={faBriefcase} /> Puestos</li>
+                      <li onClick={() => handleClick('departamento')}><FontAwesomeIcon icon={faBuilding} /> Departamentos</li>
+                      <li onClick={() => handleClick('rol')}><FontAwesomeIcon icon={faUserShield} /> Roles</li>
+                      <li onClick={() => handleClick('anticipo')}><FontAwesomeIcon icon={faFileInvoiceDollar} /> Anticipos</li>
+                      <li onClick={() => handleClick('Igss')}><FontAwesomeIcon icon={faLandmark} /> IGSS P</li>
+                      <li onClick={() => handleClick('bonificacion')}><FontAwesomeIcon icon={faGift} /> Bonificaciones</li>
+                      <li onClick={() => handleClick('asistencia')}><FontAwesomeIcon icon={faClock} /> Asistencias</li>
+                      <li onClick={() => handleClick('nominas')}><FontAwesomeIcon icon={faMoneyCheckAlt} /> Nóminas</li>
+                      <li onClick={() => handleClick('permisos')}><FontAwesomeIcon icon={faUserShield} /> Permisos</li>
+                      <li onClick={() => handleClick('isr')}><FontAwesomeIcon icon={faUserShield} /> Calculo ISR</li>
+                    </>
+                  )}
+                </ul>
+              )}
+            </>
+          )}
+
+          {(role === 'Admin' || role === 'RRHH') && (
+            <>
+              <li onClick={() => setNominaOpen(!nominaOpen)}>
+                <FontAwesomeIcon icon={faMoneyCheckAlt} /> Nómina {nominaOpen ? <FontAwesomeIcon icon={faChevronUp} /> : <FontAwesomeIcon icon={faChevronDown} />}
+              </li>
+              {nominaOpen && (
+                <ul className="submenu">
+                  <li onClick={() => handleClick('detallenominaM')}><FontAwesomeIcon icon={faFileInvoiceDollar} /> Nómina Mensual</li>
+                  <li onClick={() => handleClick('detallenominaQ')}><FontAwesomeIcon icon={faFileInvoiceDollar} /> Nómina Quincenal</li>
+                  <li onClick={() => handleClick('detallenominaS')}><FontAwesomeIcon icon={faFileInvoiceDollar} /> Nómina Semanal</li>
+                  <li onClick={() => handleClick('periodo')}><FontAwesomeIcon icon={faCalendarAlt} /> Periodos de Nómina</li>
+              <li onClick={() => handleClick('planilla')}><FontAwesomeIcon icon={faUserShield} /> Planilla Aguinaldo</li>
+              <li onClick={() => handleClick('planilla14')}><FontAwesomeIcon icon={faUserShield} /> Planilla Bono 14</li>
+              <li onClick={() => handleClick('finiquito')}><FontAwesomeIcon icon={faUserShield} /> Finiquito</li>
+         
+                </ul>
+              )}
+            </>
+          )}
+
+          <li onClick={handleLogout}>
+            <FontAwesomeIcon icon={faSignOutAlt} /> Cerrar sesión
+          </li>
+        </ul>
+      )}
+    </div>
+  );
+}
+
+export default Sidebar;
